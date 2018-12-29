8397a82d7336f43d2f7bfa742af424e92bfa80b0
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-12-29 13:32:32 +0000

    version 8.0 updated

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var stuff = require("./stuff")
 console.log(stuff.counter(["Joe", "Christian", "Brett"]));
  
  
-console.log(stuff.adder(10, 12));
+console.log(stuff.adder(stuff.pi, 12));
 
 console.log(stuff.pi);
-
