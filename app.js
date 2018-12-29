c65f31b53e6f3331a8ef8bb6b05723919356cca8
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-12-29 14:36:23 +0000

    Version 10.0

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 var fs = require("fs");
 
-
-fs.readFile("readme.txt", "utf8", function(err, data){
-    console.log("test1");
-    if(err) {
-        console.log("error!");
-    } else {
-        console.log("test2");
-        fs.writeFile("writeme.txt", data);
-    }
-});
-
-// fs.writeFileSync("writeme.txt", readMe);
-console.log("test3");
